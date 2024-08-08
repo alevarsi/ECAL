8626465542184583642df4d5b3eaa2ae69c45143
Alessandro Varsi <varsialessandro@gmail.com>
2024-08-08 15:16:19 +0200

    upload

--- runDump-2_reco.py
+++ runDump-2_reco.py
@@ -6,14 +6,9 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 options = VarParsing ('analysis')
 options.parseArguments()
 
-#import EventFilter.ESRawToDigi.esRawToDigi_cfi
-#ecalPreshowerDigis = EventFilter.ESRawToDigi.esRawToDigi_cfi.esRawToDigi.clone()
-
-process = cms.Process('RECO')
+process = cms.Process('EcalZee')
 
 # import of standard configurations
-
-#new
 process.load('Configuration.StandardSequences.L1Reco_cff')
 process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
 
@@ -23,11 +18,10 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
-process.load('Configuration.StandardSequences.EndOfProcess_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
 process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 
 process.maxEvents = cms.untracked.PSet(
@@ -41,7 +35,7 @@ process.TFileService = cms.Service("TFileService",
 )
 
 #process.options = cms.untracked.PSet(
-#    SkipEvent = cms.untracked.vstring('ProductNotFound'),
+ #  SkipEvent = cms.untracked.vstring('ProductNotFound'),
 #)
 
 # Production Info
@@ -53,6 +47,34 @@ process.configurationMetadata = cms.untracked.PSet(
 
 # Other statements
 from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, '92X_dataRun2_2017Repro_Candidate_2017_11_10_15_04_54', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '92X_dataRun2_Prompt_v9', '')
+
+
+
+
+
+#--------------------------
+# Define PAT sequence
+#--------------------------
+
+# Standard PAT Configuration File
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+process.patElectrons.addElectronID = cms.bool(False)
+
+# ---- remove MC references ----
+
+from PhysicsTools.PatAlgos.tools.coreTools import removeMCMatching
+removeMCMatching(process, ['All'], outputModules=[], postfix="")
+#removeMCMatching(process, ['All'])
+process.cleanPatTaus.preselection = cms.string('tauID("decayModeFinding") > 0.5 & tauID("byLooseCombinedIsolationDeltaBetaCorr3Hits") > 0.5 & tauID("againstMuonTight3") > 0.5 ')
+
+
+process.patMETs.addGenMET = cms.bool(False)
+
+process.options.allowUnscheduled = cms.untracked.bool(True)
+
+
 
 # Output definition
 
@@ -62,34 +84,53 @@ process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
         filterName = cms.untracked.string('')
     ),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
-    fileName = cms.untracked.string('test_RAW2DIGI2RECO.root'),
-    #outputCommands = process.RECOSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('reco_RAW2DIGI_RECO.root'),
+   # outputCommands = process.RECOSIMEventContent.outputCommands,
     outputCommands= cms.untracked.vstring("drop *",
                                           'keep *_ecal*_*_*'),
     splitLevel = cms.untracked.int32(0)
 )
 
-process.GlobalTag = GlobalTag(process.GlobalTag, '140X_dataRun3_v3', '') # 
-# https://twiki.cern.ch/twiki/bin/view/CMSPublic/GTsRun3
-# https://cms-conddb.cern.ch/cmsDbBrowser/search/Prod/140X_dataRun3_v3
-#https://github.com/cms-sw/cmssw/blob/CMSSW_11_1_X/Configuration/AlCa/python/autoCond.py
 
-#process.GlobalTag.toGet = cms.VPSet(
-              #cms.PSet(record = cms.string("EcalPedestalsRcd"),
-                       #tag =  cms.string("EcalPedestals_hlt"),
-                       #connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
-                       #),
-#)
+#--------------------------
+# Define RECO sequence
+#--------------------------
 
-#process.PulseTreeProducer = cms.EDAnalyzer('ESPulseDumper',
- #                           ESdigiCollection = cms.InputTag("ecalPreshowerDigis")
-  #                         )
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
 
-#edm::SortedCollection<EcalUncalibratedRecHit,edm::StrictWeakOrdering<EcalUncalibratedRecHit> >    "ecalMultiFitUncalibRecHit"   "EcalUncalibRecHitsEB"   "RECO"    
-#EBDigiCollection                      "ecalDigis"                 "ebDigis"         "RECO"    
+##################################
+#### costumization for Stage2 ####
+
+from HLTrigger.Configuration.customizeHLTforALL import customizeHLTforAll
+#process = customizeHLTforAll(process,"GRun",_customInfo)
+
+from HLTrigger.Configuration.customizeHLTforCMSSW import customizeHLTforCMSSW
+process = customizeHLTforCMSSW(process,"GRun")
 
 
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+
+
+#--------------------------
+# Tree producer
+#--------------------------
+
+
+#process.TreeProducer = cms.EDAnalyzer('TreeProducer',
+ #                           EleTag    = cms.InputTag("patElectrons"),
+  #                          vtxTag    = cms.InputTag("goodPrimaryVertices"),
+   #                         SuperClusterEBTag    = cms.InputTag("particleFlowSuperClusterECAL:particleFlowSuperClusterECALBarrel"),
+    #                        SuperClusterEETag    = cms.InputTag("particleFlowSuperClusterECAL:particleFlowSuperClusterECALEndcapWithPreshower"),
+     #                      )
+
+#vector<reco::SuperCluster>            "particleFlowSuperClusterECAL"   "particleFlowSuperClusterECALBarrel"   "RECO"    
+#vector<reco::SuperCluster>            "particleFlowSuperClusterECAL"   "particleFlowSuperClusterECALEndcapWithPreshower"   "RECO"    
+
+
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(50)
 
 process.source = cms.Source("PoolSource",
                                 fileNames = cms.untracked.vstring(options.inputFiles),
@@ -97,37 +138,40 @@ process.source = cms.Source("PoolSource",
                                 )
 
 
-process.raw2digi_step = cms.Path(process.RawToDigi)
-process.reconstruction_step = cms.Path(process.reconstruction) #new
-process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
-process.endjob_step = cms.EndPath(process.endOfProcess)
+#process.TreeProducer_step = cms.Path(
+ #      process.patDefaultSequence *
+  #     process.TreeProducer
+   #    )
 
 
-RawToDigiTask = cms.Task(
-                         ##L1TRawToDigiTask,
-                         #siPixelDigisTask,
-                         ##siStripDigis,
-                         #ecalDigisTask,
-                         #ecalPreshowerDigis,
-                         ##hcalDigis,
-                         ##muonCSCDigis,
-                         ##muonDTDigis,
-                         ##muonRPCDigis,
-                         ##castorDigis,
-                         ##scalersRawToDigi,
-                         ##tcdsDigis,
-                         ##onlineMetaDataDigis,
-                         )
-
-#RawToDigi = cms.Sequence(RawToDigiTask)
-#process.PulseTreeProducer_step = cms.Path(process.PulseTreeProducer)
-
+#
+# Schedule definition
+#
 process.schedule = cms.Schedule(
-                                process.raw2digi_step,
-                                process.reconstruction_step,
-                                #process.PulseTreeProducer_step, 
-                                process.endjob_step,
-                                process.RECOSIMoutput_step
-                                )
+                       process.raw2digi_step,
+                       process.reconstruction_step,
+                       process.endjob_step,
+                       #process.TreeProducer_step,
+                       process.RECOSIMoutput_step
+                       )
+
+#process.schedule = cms.Schedule(process.pEcalAlignment)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+process.options.numberOfThreads=cms.untracked.uint32(4)
+process.options.numberOfStreams=cms.untracked.uint32(0)
 
 
+
+# Customisation from command line
+from Configuration.DataProcessing.RecoTLR import customiseDataRun2Common 
+
+#call to customisation function customiseDataRun2Common imported from Configuration.DataProcessing.RecoTLR
+process = customiseDataRun2Common(process)
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
